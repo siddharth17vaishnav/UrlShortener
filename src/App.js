7620228c11afc21d467eb40ab931c33bd865e422
siddharth vaishnav <siddharth17vaishnav@gmail.com>
2021-08-09 18:21:50 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState,useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import { Button, Container ,TextField} from '@material-ui/core';
 
-function App() {
+const App = () =>{
+
+const [url,setUrl]=useState([]);
+const [data,setData]=useState([]);
+
+
+
+  const urlshort=`/api/api.php?key=d4b25bf4fc52d7571f816d2af67d2576ff265&short=${data}`;
+ const api= axios.get(urlshort).then(res=>{
+    const output= res.data.url.shortLink;
+    setUrl(output);
+    console.log(output);
+  });
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+      <Container maxWidth="sm" className="AppContainer" >
+
+      <TextField className="textfiled"
+          id="outlined-number"
+          label="Link"
+          type="text"
+          variant="outlined"
+          onChange={e=>setData(e.target.value)}
+        />
+<div className="btn">
+<Button variant="contained" color="primary" onClick={api}>
+  Submit
+</Button>
+        </div>
+
+      {url && <div> ShortLink:<a href={url}>click here</a></div>}
+      
+      </Container>
+      </div>
+     
   );
 }
 
