4d5e2c1ba6ecfb08f928073c7e27e51f17ed3fb5
siddharth vaishnav <siddharth17vaishnav@gmail.com>
2021-08-11 10:56:24 +0530

    five

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState} from 'react';
 import axios from 'axios';
 import './App.css';
 import { Button, Container ,TextField} from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const App = () =>{
 
@@ -12,12 +14,13 @@ const [query,setQuery]=useState('');
 
 const getData=async() =>{
   const api= await axios.get(`/api/api.php?key=d4b25bf4fc52d7571f816d2af67d2576ff265&short=${query}`).then(res=>{
-    const output= res.data.url.shortLink;
+    const output= res.data.url;
     setUrl(output);
 
     
 })
 }
+
 
 const getsearch=(e)=> {
 e.preventDefault();
@@ -28,7 +31,7 @@ setQuery(e.target.value);
      
       <Container maxWidth="sm" className="AppContainer" >
      
-
+<div className="text">
       <TextField className="textfiled"
           id="outlined-number"
           label="Link"
@@ -36,16 +39,27 @@ setQuery(e.target.value);
           variant="outlined"
           onChange={getsearch}
         />
+        </div>
         <div className="btn">
         <Button variant="contained" color="primary" onClick={getData}>
           Submit
         </Button>
+        <div className="card">
+        <Card >
+      <CardContent>
+              <span>Title :  {url.title} </span>
+              <br></br>
+              <span>ShortLink : <a href={url.shortLink} target="_blank"> {url.shortLink}</a></span>
+              <br></br>
+              <span>FullLink :  {url.fullLink}</span>
+        
+      </CardContent>
+    </Card>
+    </div>
                 </div>
 
-              {url && <div className="link"> ShortLink :  {url}</div>}
-
+               
            
-              
               </Container>
               </div>
             
